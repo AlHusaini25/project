8fd748fcb2bf20f67735d7e8da3674ae3d457de7
Alhusainiw <albknbgalll@gmail.com>
2025-12-15 18:42:36 +0700

    views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/coba', 'Coba::index');
-$routes->get('/coba/(:any)', 'Coba::about/$1')
-$routes->get('coba/about', 'Coba::about');
+$routes->get('pages', 'Pages::index');
+$routes->get('pages/index', 'Pages::index');
+
 
